5145ece8919a88720f1ecca7b37c202ae13b9610
Kartikey Rana <44106280+kartikey09@users.noreply.github.com>
2025-05-20 14:58:47 +0530

    20/05/2025

---
+++ leetcode/src/leetcode/Solution20_05_0251.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.*;
+
+class Solution20_05_0251 {
+	public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        for(int i : nums){
+            hs.merge(i, 1, (o, n)-> o + n);
+            if(hs.size() > 2)
+                for(Map.Entry<Integer, Integer> entry : hs.entrySet())
+                    entry.setValue(entry.getValue() - 1);
+        }
+
+        List<Integer> ans = new LinkedList<>();
+        if(hs.size() == 0)
+            return ans;
+        else {
+              for(int k : hs.keySet())
+                hs.put(k, 0);
+
+            for(int k : nums)
+                hs.merge(k, 1, (o, n)-> o+n);
+                
+            for(Map.Entry<Integer, Integer> entry : hs.entrySet()){
+                if(entry.getValue() > nums.length/3)
+                    ans.add(entry.getKey());
+            }
+        }
+        return ans;
+    }
+
+}
---
+++ leetcode/src/leetcode/main.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
