87f8ac0e289ad6d083409061fc24c0b75a5c4a93
Kartikey Rana <44106280+kartikey09@users.noreply.github.com>
2025-06-04 14:50:01 +0530

    1922. Count Good Numbers

---
+++ leetcode/src/Recursion/Solution04_06__248.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Solution04_06__248 {
+	int mod = (int)1e9+7;
+    public int countGoodNumbers(long n) {
+        if(n == 1) return 5;
+        if(n%2 == 0)
+            return (int)((getPow(20, n/2))%mod);
+        else 
+            return (int)(((getPow(20, n/2)%mod) * 5)%mod);
+    }
+    long getPow(long x, long n){
+        if(n == 0) return 1;
+
+        if(n%2 == 0)
+            return getPow((x*x) % mod, n/2)%mod;
+        else 
+            return (x*getPow((x*x) % mod, n/2))%mod;
+    }
+
+}
