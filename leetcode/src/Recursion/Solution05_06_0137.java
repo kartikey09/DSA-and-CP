617c0edfe659ce8e5f653ae9774c82245b7456d9
Kartikey Rana <44106280+kartikey09@users.noreply.github.com>
2025-06-05 13:38:10 +0530

    22. Generate Parentheses

---
+++ leetcode/src/Recursion/Solution05_06_0137.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution05_06_0137 {
+	public List<String> generateParenthesis(int n) {
+        LinkedList<String> ans = new LinkedList<>();
+        getSol(0, 0, n, "", ans);
+        return ans;
+    }
+
+    void getSol(int open, int close, int target, String ansSoFar, LinkedList<String> ansList){
+        if(open == close && close == target){
+            ansList.add(ansSoFar);
+            return;
+        }
+
+        if(open != target){
+            getSol(open+1, close, target, ansSoFar + "(", ansList);
+        }
+
+        if(close < open){
+            getSol(open, close+1, target, ansSoFar + ")", ansList);
+        }
+
+    }
+}
